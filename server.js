9988b998a3da606cb26a17516b76b54e569b2708
qzy <zhaoyingqiao1996@gmail.com>
2021-04-05 23:39:16 +0800

    adding database connection

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const app = express();
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'abc',
-        database: 'smartbrain'
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
