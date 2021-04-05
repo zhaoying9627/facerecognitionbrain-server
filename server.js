2113a30de683f8c25fbb43232433f18c3594cc0b
qzy <zhaoyingqiao1996@gmail.com>
2021-04-05 20:33:32 +0800

    change port

--- server.js
+++ server.js
@@ -35,4 +35,4 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(3001 || process.env.PORT);
+app.listen(process.env.PORT || 3001);
